0b5effc0b2ea88125dc0fcef79d9e14d32478bd4
Gregory <gpzim98@gmail.com>
2021-03-18 12:40:54 +0000

    Ignoring local config

--- local_config.py
+++ local_config.py
@@ -1,8 +1,3 @@
 DB_NAME = "db.db"
 ARQUIVE_FILES = "D:\\Personal\\monitor\\processed_files"
 ARQUIVE_FILED_FILES = "D:\\Personal\\monitor\\failed_files"
-
-try:
-    from local_config import *
-except Exception as e:
-    pass
